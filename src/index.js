8e09ab26f745c1bc9ee93d08b28a87633abc9825
Igor Valnev <dx89@mail.ru>
2019-10-07 18:05:30 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = 
+function check(str, bracketsConfig) {
+  let a = 0;
+  let b = 0;
+  let c = str.split("");
+  let d = true;
+
+  c.forEach(element => {
+    if (element === "(") {
+      a++;
+    }
+    if (element === ")") {
+      a--;
+      if (a < 0) {
+        d = false;
+      }
+    }
+    if (element === "[") {
+      b++;
+    }
+    if (element === "]") {
+      b--;
+      if (b < 0) {
+        d = false;
+      }
+    }
+  });
+
+  return d;
 }
+
+// console.log(check('())(', [['(', ')']]));
